3bc909fd4573576e82f1aadb084eb8993412f6b9
itaseski <itaseski@t.mk>
2022-01-29 10:13:30 +0100

    using variables in templates

--- reviews/views.py
+++ reviews/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'reviews/base.html')
+    name="world"
+    return render(request, 'reviews/base.html', {'name':name})
 

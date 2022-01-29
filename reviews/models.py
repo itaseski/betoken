2839da4a8d84b79ceb873fe1316ea91f82a35b0d
itaseski <itaseski@t.mk>
2022-01-29 14:35:48 +0100

    model Publisher

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    """A company that publishes books."""
+    name=models.CharField(max_length=50, help_text='The name of Publisher')
+    web_site=models.URLField(max_length=200, help_text="The Publisher's website.")
+    email = models.EmailField(max_length=254, help_text="The Publisher's email address.")

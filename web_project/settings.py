e286bfb2b8e042ee75651749118b747942c7ee2c
itaseski <itaseski@t.mk>
2022-01-29 10:07:29 +0100

    render template

--- web_project/settings.py
+++ web_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'driver_manual.apps.DriverManualConfig',
+    'reviews.apps.ReviewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

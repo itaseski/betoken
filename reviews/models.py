0b4774a691c34c1479ba6983d5f0026e5b5cbb6a
itaseski <itaseski@t.mk>
2022-01-29 14:39:55 +0100

    maodel Book

--- reviews/models.py
+++ reviews/models.py
@@ -6,3 +6,9 @@ class Publisher(models.Model):
     name=models.CharField(max_length=50, help_text='The name of Publisher')
     web_site=models.URLField(max_length=200, help_text="The Publisher's website.")
     email = models.EmailField(max_length=254, help_text="The Publisher's email address.")
+
+class Book(models.Model):
+    """A published book."""
+    title = models.CharField(max_length=70, help_text="The title of the book.")
+    publication_date = models.DateField(verbose_name="Date the book was published.")
+    isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")

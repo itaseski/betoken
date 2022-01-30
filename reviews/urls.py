b5d8586413024d87b42d6c0fb848a9f3a6db7641
itaseski <itaseski@t.mk>
2022-01-30 15:24:19 +0100

    confused

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name="index"),
+    #path('', views.index,name="index"),
+    path('', views.BookListView.as_view(), name='article-list'),
     path('search-results', views.search_result),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from .models import Book, Review
+
+class BookListView(ListView):
+    model = Book
+    #template_name = "reviews/book_list.html"
+    context_object_name = 'books'
+    paginate_by = 100  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 def index(request):
